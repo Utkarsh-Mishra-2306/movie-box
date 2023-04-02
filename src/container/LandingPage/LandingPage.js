12628d3e328358f9c16609b59981695ac9ff6613
Utkarsh Mishra <utkarsh4u.mishra@gmail.com>
2023-04-02 19:42:51 +0530

    completed LandingPage and SignInPage

--- src/container/LandingPage/LandingPage.js
+++ src/container/LandingPage/LandingPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./LandingPage.css";
 import Navbar from "../../components/Navbar/Navbar.js";
+import LandingPageContent from "../../components/LandingPageContent/LandingPageContent.js";
 
 function LandingPage(){
     return(
         <div className = "landingpage_container">
             <Navbar />
+            <LandingPageContent />
         </div>
     );
 }
